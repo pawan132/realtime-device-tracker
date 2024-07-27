30951bc6e414e4ebde299aff512218e6e34145f3
Pawan132 <pawan.2024cse1101@kiet.edu>
2024-07-28 03:03:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express= require('express');
+
+const app = express();
+
+const path = require("path");
+
+const http= require("http");
+
+const socketio= require("socket.io");
+
+const server= http.createServer(app);
+
+const io= socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname,"public"))); //this is because we can use assets(images) & css
+
+io.on("connection", function(socket) {    //socket is unique socket key
+    socket.on("send-location",function (data) {
+        io.emit("receive-location" , {id: socket.id, ...data}); 
+    })
+
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.id);
+    })
+  
+})
+
+app.get("/", function(req,res) {
+    res.render("index");
+});
+
+server.listen(3000);
